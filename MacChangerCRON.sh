2ac40077283ccc1116aedcae21ddcb28f5c9a83f
CeramicSkate0 <CeramicSkate0@users.noreply.github.com>
2016-03-03 13:19:11 -0600

    Update MacChangerCRON.sh

--- MacChangerCRON.sh
+++ MacChangerCRON.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
-macchanger wlan0 -e
-macchanger wlan1 -e
-macchanger eth0 -e
+ifconfig eth0 down
+ifconfig wlan0 down
+ifconfig wlan1 down
+macchanger wlan0 -r -b
+macchanger wlan1 -r -b
+macchanger eth0 -r -b
+ifconfig eth0 up
+ifconfig wlan0 up
+ifconfig wlan1 up
