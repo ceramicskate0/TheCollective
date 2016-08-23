0ca4e42b35f4374295629b703121f771c54e3e95
CeramicSkate0 <CeramicSkate0@users.noreply.github.com>
2016-08-22 22:11:00 -0500

    Update InstallFreshKaliImage.sh

--- InstallFreshKaliImage.sh
+++ InstallFreshKaliImage.sh
@@ -23,7 +23,7 @@ echo "FIXED THE REPO SOURCE.LIST, AGAIN. Your Welcome!"
 #echo "#wpa-key-mgmt WPA-PSK">>interfaces
 #echo "Fixed wireless config issue, might have to reboot to get it to work."
 #----------------Fix Interface--------------------------------
-echo"Starting to grab updates (BUT NOT UPGRADE DUE TO THE UPGRADES WILL BREAK ARMITAGE AT THIS TIME)"
+echo "Starting to grab updates (BUT NOT UPGRADE DUE TO THE UPGRADES WILL BREAK ARMITAGE AT THIS TIME)"
 cd /
 apt-get update
 apt full-upgrade -y
