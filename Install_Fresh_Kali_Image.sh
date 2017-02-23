4353919f22ba22e3da506ab7c952ac04cac53bf4
CeramicSkate0 <CeramicSkate0@users.noreply.github.com>
2017-02-22 22:56:00 -0600

    Update Install_Fresh_Kali_Image.sh

--- Install_Fresh_Kali_Image.sh
+++ Install_Fresh_Kali_Image.sh
@@ -286,7 +286,7 @@ git clone https://github.com/byt3bl33d3r/pyobfuscate.git
 #iceweasel --new-tab https://www.cobaltstrike.com/download 
 #iceweasel --new-tab http://blog.didierstevens.com/my-software/
 #iceweasel --new-tab http://www.truesec.se/sakerhet/verktyg/saakerhet/gsecdump_v2.0b5
-#iceweasel --new-tab http://www.poisonivy-rat.com/
+#iceweasel --new-tab http://www.poisonivy-rat.com/index.php https://samsclass.info/123/proj10/Poison%20Ivy%202.3.2.7z
 #iceweasel --new-tab https://www.winitor.com/features.html
 #-------------------FIX missing updates form installs and fix settings-----------------------------
 apt-get update --fix-missing
