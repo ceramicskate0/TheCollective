39b596ea98da1245d42b46378f3b3b34090de6eb
CeramicSkate0 <CeramicSkate0@users.noreply.github.com>
2017-03-06 15:56:47 -0600

    Update Install_Fresh_Kali_Image.sh

--- Install_Fresh_Kali_Image.sh
+++ Install_Fresh_Kali_Image.sh
@@ -12,18 +12,6 @@ echo "#Kali 2016 Rolling Repos">>sources.list
 echo "deb http://http.kali.org/kali kali-rolling main contrib non-free">>sources.list
 echo "deb-src http://http.kali.org/kali kali-rolling main contrib non-free">>sources.list
 echo "FIXED THE REPO SOURCE.LIST, AGAIN. Your Welcome!"
-#----------------Fix wireless Interface issue--------------------------------
-#echo "Fixing Wifi issue"
-#cd /etc/network/
-#echo "#Wireless Interface setup by install script"
-#echo "#auto wlan0">>interfaces
-#echo "#iface wlan0 inet manual">>interfaces
-#echo "#iface wlan0 inet static">>interfaces
-#echo "#wpa-ssid AP_NAME">>interfaces
-#echo "#wpa-key-mgmt WPA-PSK">>interfaces
-#echo "Fixed wireless config issue, might have to reboot to get it to work."
-#----------------Fix Interface--------------------------------
-echo "Starting to grab updates (BUT NOT UPGRADE DUE TO THE UPGRADES WILL BREAK ARMITAGE AT THIS TIME)"
 cd /
 apt-get update
 apt full-upgrade -y
