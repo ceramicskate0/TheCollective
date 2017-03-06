8d46c38999d1257a99bfa2da12c8bda31906f852
CeramicSkate0 <CeramicSkate0@users.noreply.github.com>
2017-03-06 15:57:22 -0600

    Create Fix_Wifi_Config.sh

---
+++ Fix_Wifi_Config.sh
@@ -0,0 +1,11 @@
+#----------------Fix wireless Interface issue--------------------------------
+echo "Fixing Wifi issue"
+cd /etc/network/
+echo "#Wireless Interface setup by install script"
+echo "#auto wlan0">>interfaces
+echo "#iface wlan0 inet manual">>interfaces
+echo "#iface wlan0 inet static">>interfaces
+echo "#wpa-ssid AP_NAME">>interfaces
+echo "#wpa-key-mgmt WPA-PSK">>interfaces
+echo "Fixed wireless config issue, might have to reboot to get it to work."
+#----------------Fix Interface--------------------------------
