47ef3a3a63d08c2e107916dafb1c478fb4fb9c53
CeramicSkate0 <CeramicSkate0@users.noreply.github.com>
2017-04-21 13:35:09 -0500

    Update and rename Install_Standalone_Powershell.sh to Install_Powershell.sh

---
+++ Install_Powershell.sh
@@ -0,0 +1,11 @@
+apt-get update
+apt-get upgrdae -y
+wget http://security.ubuntu.com/ubuntu/pool/main/i/icu/libicu55_55.1-7ubuntu0.1_amd64.deb
+dpkg -i dpkg -i libicu55_55.1-7ubuntu0.1_amd64.deb 
+
+
+
+#standalone powershell exe
+#curl -OL https://github.com/PowerShell/PowerShell/releases/download/v6.0.0-alpha.17/PowerShell-x86_64.AppImage
+#chmod 777 ./PowerShell-x86_64.AppImage
+#./PowerShell-x86_64.AppImage
--- Install_Standalone_Powershell.sh
+++
@@ -1,3 +0,0 @@
-curl -OL https://github.com/PowerShell/PowerShell/releases/download/v6.0.0-alpha.17/PowerShell-x86_64.AppImage
-chmod 777 ./PowerShell-x86_64.AppImage
-./PowerShell-x86_64.AppImage
