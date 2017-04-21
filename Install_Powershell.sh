57f0fed14808bcad5edb05f9c72c480a3c2409ce
CeramicSkate0 <CeramicSkate0@users.noreply.github.com>
2017-04-21 13:38:10 -0500

    Update Install_Powershell.sh

--- Install_Powershell.sh
+++ Install_Powershell.sh
@@ -1,9 +1,11 @@
+#!/bin/bash
 apt-get update
 apt-get upgrdae -y
 wget http://security.ubuntu.com/ubuntu/pool/main/i/icu/libicu55_55.1-7ubuntu0.1_amd64.deb
-dpkg -i dpkg -i libicu55_55.1-7ubuntu0.1_amd64.deb 
-
-
+dpkg -i dpkg -i libicu55_55.1-7ubuntu0.1_amd64.deb
+git clone https://github.com/PowerShell/PowerShell/releases/download/v6.0.0-alpha.18/powershell_6.0.0-alpha.18-1ubuntu1.16.04.1_amd64.deb
+dpkg -i powershell_6.0.0-alpha.18-1ubuntu1.14.04.1_amd64.deb
+apt-get install -f
 
 #standalone powershell exe
 #curl -OL https://github.com/PowerShell/PowerShell/releases/download/v6.0.0-alpha.17/PowerShell-x86_64.AppImage
