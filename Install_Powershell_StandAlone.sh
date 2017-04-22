4f74541e8ccad0e7bc70f4c6276da2860a3f3279
CeramicSkate0 <CeramicSkate0@users.noreply.github.com>
2017-04-21 20:24:24 -0500

    Create Install_Powershell_StandAlone.sh

---
+++ Install_Powershell_StandAlone.sh
@@ -0,0 +1,4 @@
+#standalone powershell binary
+curl -OL https://github.com/PowerShell/PowerShell/releases/download/v6.0.0-alpha.17/PowerShell-x86_64.AppImage
+chmod 777 ./PowerShell-x86_64.AppImage
+./PowerShell-x86_64.AppImage
