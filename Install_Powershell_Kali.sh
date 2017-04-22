6a68681bd6dae09e94b7d260a57ef26ce6961aa8
CeramicSkate0 <CeramicSkate0@users.noreply.github.com>
2017-04-21 20:26:25 -0500

    Update Install_Powershell_Kali.sh

--- Install_Powershell_Kali.sh
+++ Install_Powershell_Kali.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-apt-get update
-apt-get upgrdae -y
 wget http://security.ubuntu.com/ubuntu/pool/main/i/icu/libicu55_55.1-7ubuntu0.1_amd64.deb
 dpkg -i dpkg -i libicu55_55.1-7ubuntu0.1_amd64.deb
+apt-get install -f
+wget http://security.debian.org/debian-security/pool/updates/main/o/openssl/libssl1.0.0_1.0.1t-1+deb7u2_amd64.deb
+dpkg -i libssl1.0.0_1.0.1t-1+deb7u2_i386.deb
+apt-get install -f
 wget https://github.com/PowerShell/PowerShell/releases/download/v6.0.0-alpha.18/powershell_6.0.0-alpha.18-1ubuntu1.16.04.1_amd64.deb
 dpkg -i powershell_6.0.0-alpha.18-1ubuntu1.14.04.1_amd64.deb
 apt-get install -f
-
-#standalone powershell binary
-#curl -OL https://github.com/PowerShell/PowerShell/releases/download/v6.0.0-alpha.17/PowerShell-x86_64.AppImage
-#chmod 777 ./PowerShell-x86_64.AppImage
-#./PowerShell-x86_64.AppImage
+rm powershell_6.0.0-alpha.18-1ubuntu1.14.04.1_amd64.deb
+rm libicu55_55.1-7ubuntu0.1_amd64.deb
+rm libssl1.0.0_1.0.1t-1+deb7u2_amd64.deb
