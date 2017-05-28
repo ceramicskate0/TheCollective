98effecab3fbb872afcf821eb9fee75306368cd0
ceramicskate0 <ceramicskate0@users.noreply.github.com>
2017-05-28 18:42:28 -0500

    installer

---
+++ install.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+import os
+
+currentdir=os.getcwd()
+files = [x for x in os.listdir(currentdir+str("/Lists")) if x.endswith('.txt')]
+if (os.path.isdir(str(currentdir)+"/ToolBox")) is False:
+	os.makedir(str(currentdir)+"/ToolBox")
+os.system("cd "+str(currentdir)+ "/Lists")
+for filename in files:
+	print "--Starting work on "+str(filename)
+	dirname= os.path.splitext(str(filename))[0]
+	os.system("cd "+str(currentdir)+"/ToolBox/ && mkdir "+str(dirname))
+	f=open(str(currentdir)+ "/Lists/"+str(filename))
+	line=f.readline()
+	while line:
+		os.system("cd "+str(currentdir)+"/ToolBox/"+str(dirname) +" && "+line)
+		line=f.readline()
+	f.close()
