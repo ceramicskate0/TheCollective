5e3f845aac7bd86d78fcec4c27a9459faeb44917
ceramicskate0 <ceramicskate0@users.noreply.github.com>
2017-05-30 20:12:32 -0500

    Update update.py

--- update.py
+++ update.py
@@ -2,7 +2,7 @@
 import os
 
 os.system("cd ..")
-os.system("rmdir YAKSR")
+os.system("rm -rf YAKSR")
 os.system("git clone https://github.com/ceramicskate0/YAKSR")
 os.system("cd YAKSR")        
 os.system("chmod +777 install.py")
