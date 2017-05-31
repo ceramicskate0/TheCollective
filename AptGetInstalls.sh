a5ec8be68dd7023617eee3ed126122767f936c56
ceramicskate0 <ceramicskate0@users.noreply.github.com>
2017-05-30 19:42:37 -0500

    Update AptGetInstalls.sh

--- AptGetInstalls.sh
+++ AptGetInstalls.sh
@@ -1,4 +1,24 @@
 #!/bin/bash
+echo " ">/etc/apt/sources.list
+echo "#KALI 2.0 Repos">>/etc/apt/sources.list
+echo "#deb http://http.kali.org/kali sana main non-free contrib">>/etc/apt/sources.list
+echo "#deb http://security.kali.org/kali-security sana/updates main contrib non-free">>/etc/apt/sources.list
+echo "#deb-src http://http.kali.org/kali sana main non-free contrib">>/etc/apt/sources.list
+echo "#deb-src http://security.kali.org/kali-security sana/updates main contrib non-free">>/etc/apt/sources.list
+echo "#Kali 2016 Rolling Repos">>/etc/apt/sources.list
+echo "deb http://http.kali.org/kali kali-rolling main contrib non-free">>/etc/apt/sources.list
+echo "deb-src http://http.kali.org/kali kali-rolling main contrib non-free">>/etc/apt/sources.list
+apt-get update
+apt full-upgrade -y
+apt-get dist-upgrade -y 
+apt-get upgrade -y
+update-rc.d postgresql enable && update-rc.d metasploit enable && msfdb init
+msfupdate
+apt-get update --fix-missing
+apt-get -f install
+apt-get install --reinstall gnome-control-center --fix-missing
+apt autoremove -y
+apt-get clean
 apt-get install Beef -y
 apt-get install hping3 -y
 apt-get install powersploit -y
@@ -50,3 +70,20 @@ apt-get install libreoffice -y
 apt-get install knotes -y
 apt-get install remmina -y
 apt-get install freerdp-x11 -y
+apt-get -y install ldap-utils rwho rsh-client x11-apps finger
+apt-get -y install lbd automater arachni bully inguma sslsplit dumpzilla -y 
+apt-get -y install owasp-mantra-ff recon-ng ridenum regripper jd-gui -y
+apt-get -y install metagoofil
+apt-get -y install unicornscan enum4linux polenum
+apt-get -y install nfspy firmware-mod-kit wmis
+apt-get -y install unicornscan
+apt-get -y install openvas
+apt-get -y install hping3
+apt-get -y install nipper-ng jsql oclgausscrack ghost-phisher uniscan -y
+apt-get -y install bleachbit
+apt-get -y install file-roller
+apt-get -y install htop
+apt-get -y install shellter
+apt-get -y install jboss-autopwn
+apt-get -y install wifite
+apt-get install fruitywifi -y
