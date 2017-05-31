2171ff837d08b5eac4d792b813da9ca3e3f17859
ceramicskate0 <ceramicskate0@users.noreply.github.com>
2017-05-30 19:47:15 -0500

    Update install.py

--- install.py
+++ install.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python
 import os
 
-os.system("apt-get update")
-os.system("apt-get upgrade -y")
 currentdir=os.getcwd()
+os.chdir(str(currentdir))
+os.system("chmod +777 AptGetInstalls.sh")
+os.system("./AptGetInstalls.sh")
 files = [x for x in os.listdir(currentdir+str("/Lists")) if x.endswith('.txt')]
 if (os.path.isdir(str(currentdir)+"/ToolBox")) is False:
 	os.makedirs(str(currentdir)+"/ToolBox")
@@ -11,10 +12,14 @@ os.system("cd "+str(currentdir)+ "/Lists")
 for filename in files:
 	print "--Starting work on "+str(filename)
 	dirname= os.path.splitext(str(filename))[0]
-	os.system("cd "+str(currentdir)+"/ToolBox/ && mkdir "+str(dirname))
+	os.chdir(str(currentdir)+"/ToolBox/")
+	if (os.path.isdir(str(currentdir)+"/ToolBox/")) is False:
+			os.system("rm -rf "+str(dirname))
+			os.makedirs(str(currentdir)+"/ToolBox")
 	f=open(str(currentdir)+ "/Lists/"+str(filename))
 	line=f.readline()
+	os.chdir(str(currentdir)+"/ToolBox/"+str(dirname))
 	while line:
-		os.system("cd "+str(currentdir)+"/ToolBox/"+str(dirname) +" && "+line)
+		os.system(str(line))
 		line=f.readline()
 	f.close()
