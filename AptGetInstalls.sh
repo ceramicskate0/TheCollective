78bcc106915e3e93a8a5678129a70f1cd449db62
ceramicskate0 <ceramicskate0@users.noreply.github.com>
2017-06-01 10:50:03 -0500

    Update AptGetInstalls.sh

--- AptGetInstalls.sh
+++ AptGetInstalls.sh
@@ -43,7 +43,7 @@ apt-get install network-manager-pptp-gnome -y
 apt-get install network-manager-strongswan -y
 apt-get install network-manager-vpnc -y
 apt-get install network-manager-vpnc-gnome -y
-apt-get install unrar unace rar unrar p7zip zip unzip p7zip-full p7zip-rar file-roller
+apt-get install unrar unace rar unrar p7zip zip unzip p7zip-full p7zip-rar file-roller -y
 apt-get install gdebi -y
 apt-get install deluge -y
 apt-get install libssl-dev -y
@@ -71,7 +71,7 @@ apt-get install knotes -y
 apt-get install remmina -y
 apt-get install freerdp-x11 -y
 apt-get -y install ldap-utils rwho rsh-client x11-apps finger
-apt-get -y install lbd automater arachni bully inguma sslsplit dumpzilla -y 
+apt-get -y install lbd automater arachni bully inguma sslsplit dumpzilla
 apt-get -y install owasp-mantra-ff recon-ng ridenum regripper jd-gui -y
 apt-get -y install metagoofil
 apt-get -y install unicornscan enum4linux polenum
@@ -79,7 +79,7 @@ apt-get -y install nfspy firmware-mod-kit wmis
 apt-get -y install unicornscan
 apt-get -y install openvas
 apt-get -y install hping3
-apt-get -y install nipper-ng jsql oclgausscrack ghost-phisher uniscan -y
+apt-get -y install nipper-ng jsql oclgausscrack ghost-phisher uniscan
 apt-get -y install bleachbit
 apt-get -y install file-roller
 apt-get -y install htop
