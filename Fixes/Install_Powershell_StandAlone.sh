ce9c22b0ae48097364a8d0db71e8879b67066de2
ceramicskate0 <ceramicskate0@users.noreply.github.com>
2017-07-02 19:12:17 -0500

    Delete Install_Powershell_StandAlone.sh

--- Fixes/Install_Powershell_StandAlone.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-#standalone powershell binary
-curl -OL https://github.com/PowerShell/PowerShell/releases/download/v6.0.0-alpha.17/PowerShell-x86_64.AppImage
-chmod 777 ./PowerShell-x86_64.AppImage
-./PowerShell-x86_64.AppImage
