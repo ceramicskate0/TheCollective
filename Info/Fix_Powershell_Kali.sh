b8c2f5f0a090b4477e8c013daa62738013658f88
Ceramicskate0 <ceramicskate0@users.noreply.github.com>
2018-01-09 18:51:36 -0600

    Update Fix_Powershell_Kali.sh

--- Info/Fix_Powershell_Kali.sh
+++ Info/Fix_Powershell_Kali.sh
@@ -1,15 +1,24 @@
 #!/bin/bash
-wget http://security.ubuntu.com/ubuntu/pool/main/i/icu/libicu55_55.1-7ubuntu0.1_amd64.deb
-dpkg -i dpkg -i libicu55_55.1-7ubuntu0.1_amd64.deb
-wget http://security.debian.org/debian-security/pool/updates/main/o/openssl/libssl1.0.0_1.0.1t-1+deb7u2_amd64.deb
-dpkg -i libssl1.0.0_1.0.1t-1+deb7u2_i386.deb
-apt-get install libunwind8
-wget https://github.com/PowerShell/PowerShell/releases/download/v6.0.0-alpha.18/powershell_6.0.0-alpha.18-1ubuntu1.16.04.1_amd64.deb
-dpkg -i powershell_6.0.0-alpha.18-1ubuntu1.14.04.1_amd64.deb
+wget https://github.com/PowerShell/PowerShell/blob/master/docs/installation/linux.md
 
-rm powershell_6.0.0-alpha.18-1ubuntu1.14.04.1_amd64.deb
-rm libicu55_55.1-7ubuntu0.1_amd64.deb
-rm libssl1.0.0_1.0.1t-1+deb7u2_amd64.deb
+# Install prerequisites
+apt-get install libunwind8 libicu55
+wget http://security.debian.org/debian-security/pool/updates/main/o/openssl/libssl1.0.0_1.0.1t-1+deb8u6_amd64.deb
+dpkg -i libssl1.0.0_1.0.1t-1+deb8u6_amd64.deb
 
-powershell
+# Install PowerShell
+dpkg -i powershell_6.0.0-rc.2-1.ubuntu.16.04_amd64.deb
+
+# Start PowerShell
+pwsh
+# Grab the latest App Image
+wget https://github.com/PowerShell/PowerShell/releases/download/v6.0.0-rc.2/powershell-6.0.0-rc.2-x86_64.AppImage
+
+# Make executable
+chmod a+x powershell-6.0.0-rc.2-x86_64.AppImage
+
+# Start PowerShell
+./powershell-6.0.0-rc.2-x86_64.AppImage
+
+# Start PowerShell
 pwsh
