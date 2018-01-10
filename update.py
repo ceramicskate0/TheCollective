867242c45dc2f2a7ad6613e2851df6f4d09481e5
Ceramicskate0 <ceramicskate0@users.noreply.github.com>
2018-01-10 12:35:33 -0600

    Delete update.py

--- update.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python
-import os
-
-os.system("cd ..")
-os.system("rm -rf YAKSR")
-os.system("git clone https://github.com/ceramicskate0/YAKSR")
-os.system("cd YAKSR")        
-os.system("chmod +777 install.py")
-os.system("./install.py")
