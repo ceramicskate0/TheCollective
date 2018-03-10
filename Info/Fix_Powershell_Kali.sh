ee862e5ee6ec6e09884a45c41e30cfa833e2ec6e
Ceramicskate0 <ceramicskate0@users.noreply.github.com>
2018-03-09 23:05:12 -0600

    Update Fix_Powershell_Kali.sh

--- Info/Fix_Powershell_Kali.sh
+++ Info/Fix_Powershell_Kali.sh
@@ -1,26 +1,11 @@
 #!/bin/bash
-wget https://github.com/PowerShell/PowerShell/blob/master/docs/installation/linux.md
 
-# Install prerequisites
-apt-get install libunwind8 libicu55
-wget http://security.debian.org/debian-security/pool/updates/main/o/openssl/libssl1.0.0_1.0.1t-1+deb8u6_amd64.deb
-dpkg -i libssl1.0.0_1.0.1t-1+deb8u6_amd64.deb
-
-# Install PowerShell
-dpkg -i powershell_6.0.0-rc.2-1.ubuntu.16.04_amd64.deb
-
-# Start PowerShell
-pwsh
 # Grab the latest App Image
 wget https://github.com/PowerShell/PowerShell/releases/download/v6.0.0-rc.2/powershell-6.0.0-rc.2-x86_64.AppImage
 
 # Make executable
 chmod a+x powershell-6.0.0-rc.2-x86_64.AppImage
-mv /usr/bin/pwsh /usr/bin/powershell
- 
-# Start PowerShell
-./powershell-6.0.0-rc.2-x86_64.AppImage
+mv powershell-6.0.0-rc.2-x86_64.AppImage /usr/bin/powershell
 
 # Start PowerShell
-pwsh
 powershell
