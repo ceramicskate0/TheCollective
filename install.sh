683d0f40bea3e54cc2679bb0a3afc7ced8a47bd9
Ceramicskate0 <ceramicskate0@users.noreply.github.com>
2019-03-27 16:20:16 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -85,6 +85,7 @@ apt-get install -y  shellter
 apt-get install -y  jboss-autopwn  
 apt-get install -y  wifite  
 apt-get install -y  fruitywifi 
+apt-get install -y tesseract-ocr
 apt autoremove -y
 apt-get clean
 chmod +777 GetClones.py
